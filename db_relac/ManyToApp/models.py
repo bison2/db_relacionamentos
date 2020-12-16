6cbccdc483b15efea2df47437b391e62d2e1e73a
bison2 <profvaldes@gmail.com>
2020-12-15 22:02:28 -0200

    inclusao de relacionamentos onetone e oneto many no models.py, e alteracoes no admin.py para importar os modelos, e registrar no admin

--- db_relac/ManyToApp/models.py
+++ db_relac/ManyToApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models import CASCADE
 
 # Create your models here.
-
+#ManyToMany simples
 class Pessoa(models.Model):
     nome = models.CharField(max_length = 100)
 
@@ -17,6 +17,8 @@ class Filmes(models.Model):
     def __str__(self):
         return self.nome
 
+#ManyToMany usando uma nova tabela que tem novos campos
+
 class Programador(models.Model):
     nome = models.CharField(max_length = 100)
 
@@ -25,18 +27,29 @@ class Programador(models.Model):
 
 class Linguagem(models.Model):
     nome = models.CharField(max_length = 100)
-    programadores = models.ManyToManyField(Programador, related_name="linguagens", through = "ProgramadorLinguagem")
+    programadores = models.ManyToManyField("Programador", related_name="linguagens", through = "ProgramadorLinguagem")
 
     def __str__(self):
         return self.nome
 
 class ProgramadorLinguagem(models.Model):
-    programador = models.ForeignKey(Programador, on_delete=CASCADE, related_name="programador_linguagem")
-    linguagem = models.ForeignKey(Linguagem, on_delete=CASCADE, related_name="programador_linguagem")
+    programador = models.ForeignKey("Programador", on_delete=CASCADE, related_name="programador_linguagem")
+    linguagem = models.ForeignKey("Linguagem", on_delete=CASCADE, related_name="programador_linguagem")
 
     nivel_conhecimento = models.IntegerField()
 
     def __str__(self):
         return "{} - {} ({})".format(self.programador.nome, self.linguagem.nome, self.nivel_conhecimento)
 
+#OneToOne: o relacionamento fica na classe que não exist sem a outra
 
+class DocumentoCpf(models.Model):
+    numero = models.CharField(max_length = 20)
+    pessoa = models.OneToOneField("Pessoa")
+
+#OneToMany usa a ForeignKey no lado do "muitos" no relacionamento
+
+class Carro(models.Model):
+    nome = models.CharField(max_length = 30)
+    pessoa = models.ForeignKey("Pessoa")
+    
