607f37bbf02fc85b2e1db8952517db4e655c1559
bison2 <profvaldes@gmail.com>
2020-12-19 07:13:43 -0200

    inclusao dos formularios login, logout, register, seus templates(base.html, pasta base, auth) e mudancas na home.html. Respectivas mudancas nas views e urls.

--- db_relac/ManyToApp/admin.py
+++ db_relac/ManyToApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ManyToApp.models import Pessoa, Carro, Linguagem
+
+from .models import Pessoa, Carro, Linguagem
                         
 
                         
--- db_relac/ManyToApp/views.py
+++ db_relac/ManyToApp/views.py
@@ -4,9 +4,10 @@ from .models import Pessoa,Carro, Linguagem
 # Create your views here.
 
 def home(request):
-    pessoa_data = Pessoa.objects.all()
-    carro_data = Carro.objects.all()
+    pessoa_data = Pessoa.objects.tudo()
+    carro_data = Carro.objects.car()
     ling_data = Linguagem.objects.all()
+    
     return render(request, 'tabela/home.html', {
                     'pessoas':pessoa_data,
                     'carros': carro_data,
