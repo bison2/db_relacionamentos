893cbe8aea94ce6e11983ad43bb47cf2f7b6ea6a
bison2 <profvaldes@gmail.com>
2020-12-22 12:41:48 -0200

    mudancas na template, na views.py para criar estilo e implantar tabela que relaciona uma com as outras.

--- db_relac/ManyToApp/urls.py
+++ db_relac/ManyToApp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from ManyToApp import views
+
+from .views import home
 
 urlpatterns =[
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', home),
 ]
--- db_relac/ManyToApp/views.py
+++ db_relac/ManyToApp/views.py
@@ -5,12 +5,19 @@ from .models import Pessoa,Carro, Linguagem
 
 def home(request):
     pessoa_data = Pessoa.objects.tudo()
+
     carro_data = Carro.objects.car()
     ling_data = Linguagem.objects.all()
-    
+   
+    p1 = Pessoa.objects.get(nome="Luzia Lisboa")
+    p1.carro
+    p1_data = p1.carro.all()
+
     return render(request, 'tabela/home.html', {
                     'pessoas':pessoa_data,
                     'carros': carro_data,
                     'linguagens': ling_data,
+                    'p1s':p1_data,
+                    'pss':p1
                     }
             )
